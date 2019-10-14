e4ed0b0916936d4294f7d3dc9542b4c752eee67d
danisimas25 <45358530+danisimas25@users.noreply.github.com>
2019-10-14 15:02:38 -0400

    mandando
    atualizado o spinner e a classe

--- TripVrCodigo/app/src/main/java/com/exemplo/tripvr/helper/FirebaseHelper.java
+++ TripVrCodigo/app/src/main/java/com/exemplo/tripvr/helper/FirebaseHelper.java
@@ -1,8 +1,30 @@
 package com.exemplo.tripvr.helper;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class FirebaseHelper {
 
+    private static DatabaseReference db;
+    private static StorageReference referenciaStorage;
 
+
+    public static DatabaseReference getFirebase(){
+        if(db==null){
+            db = FirebaseDatabase.getInstance().getReference();
+        }
+        return db;
+    }
+
+    public static StorageReference getFirebaseStorage(){
+
+        if(referenciaStorage==null){
+            referenciaStorage = FirebaseStorage.getInstance().getReference();
+        }
+        return referenciaStorage;
+    }
 
 
 
