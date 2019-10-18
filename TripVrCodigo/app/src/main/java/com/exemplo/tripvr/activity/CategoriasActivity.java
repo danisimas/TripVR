f27ccfc11ff1ba4f468129e9c7ed234968e38fd6
danisimas25 <45358530+danisimas25@users.noreply.github.com>
2019-10-18 07:50:24 -0400

    feito

--- TripVrCodigo/app/src/main/java/com/exemplo/tripvr/activity/CategoriasActivity.java
+++ TripVrCodigo/app/src/main/java/com/exemplo/tripvr/activity/CategoriasActivity.java
@@ -58,6 +58,8 @@ public class CategoriasActivity extends AppCompatActivity {
 
     private void pesquisarLocalFirebase(){
 
+
+
     }
 
 }
--- TripVrCodigo/app/src/main/java/com/exemplo/tripvr/model/Local.java
+++ TripVrCodigo/app/src/main/java/com/exemplo/tripvr/model/Local.java
@@ -2,7 +2,7 @@ package com.exemplo.tripvr.model;
 
 public class Local {
 
-    String nome,endereco,horario,id,Categoria,sobre,foto1,foto2,video,foto,nomePesquisar;
+    String nome,endereco,horario,id,Categoria,sobre,foto1,foto2,video,nomePesquisar;
 
     public String getNomePesquisar() {
         return nomePesquisar.toUpperCase();
@@ -44,14 +44,6 @@ public class Local {
         this.video = video;
     }
 
-    public String getFoto() {
-        return foto;
-    }
-
-    public void setFoto(String foto) {
-        this.foto = foto;
-    }
-
     public Local(String nome, String endereco, String horario, String id, String categoria, String sobre, String foto1, String foto2, String video, String foto) {
         this.nome = nome;
         this.endereco = endereco;
@@ -62,7 +54,6 @@ public class Local {
         this.foto1 = foto1;
         this.foto2 = foto2;
         this.video = video;
-        this.foto = foto;
     }
 
     public Local(){}
